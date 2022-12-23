5e0485fe8474f6454f9816b1c0dfb8804f2b8350
Cody Lillywhite <cody@hiremysub.com>
2022-12-23 10:15:50 -0700

    feat: tailwind components

---
+++ src/components/common/styled/Buttons.jsx
@@ -0,0 +1,5 @@
+import tw from 'tailwind-styled-components';
+
+// const generalButtonStyle = 'rounded min-w-[50px]';
+
+export const PrimaryButton = tw.button`bg-[#D92414] text-white rounded min-w-30 px-4`;
---
+++ src/components/common/styled/Headings.jsx
@@ -0,0 +1,3 @@
+import tw from 'tailwind-styled-components';
+
+export const PrimaryHeader = tw.h1`text-center font-bold text-4xl`;
